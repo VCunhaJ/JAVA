e529208e66042ab3793914e9aafd52bf21b72d13
Richard Walker <richard.walker.90@gmail.com>
2015-03-06 02:10:51 -0500

    Added the Hello World file to walker

---
+++ HelloWorld copy.java
@@ -0,0 +1,12 @@
+package hello_world_git;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO!");
+		System.out.println("I expect to see this change on GitHub!");
+		System.out.println("More changes");
+	}
+
+}
